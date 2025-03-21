2dd73615f7226fb5b0e5264814df3660763a0e1b
dhfjfig <2323154589@qq.com>
2025-03-22 01:14:42 +0800

    Update hiv_model_student.py

--- src/hiv_model_student.py
+++ src/hiv_model_student.py
@@ -3,24 +3,52 @@ import matplotlib.pyplot as plt
 
 class HIVModel:
     def __init__(self, A, alpha, B, beta):
-        # TODO: 初始化模型参数
-        pass
+        self.A = A
+        self.alpha = alpha
+        self.B = B
+        self.beta = beta
 
     def viral_load(self, time):
-        # TODO: 计算病毒载量
-        return np.zeros_like(time)
+        return self.A * np.exp(-self.alpha * time) + self.B * np.exp(-self.beta * time)
 
     def plot_model(self, time):
-        # TODO: 绘制模型曲线
-        pass
+        viral_load = self.viral_load(time)
+        plt.plot(time, viral_load, label='Model')
+        plt.xlabel('Time (days)')
+        plt.ylabel('Viral Load')
+        plt.title('HIV Viral Load Model Fitting')
+        plt.legend()
+        plt.show()
 
 def load_hiv_data(filepath):
-    # TODO: 加载HIV数据
-    return np.array([]), np.array([])
+    try:
+        data = np.load(filepath)
+        time_data = data['time_in_days']
+        viral_load_data = data['viral_load']
+        return time_data, viral_load_data
+    except:
+        data = np.loadtxt(filepath, delimiter=',')
+        time_data = data[:, 0]
+        viral_load_data = data[:, 1]
+        return time_data, viral_load_data
 
 def main():
-    # TODO: 主函数，用于测试模型
-    pass
+    # 初始化模型参数
+    model = HIVModel(A=1000, alpha=0.5, B=500, beta=0.1)
+    
+    # 生成时间序列
+    time = np.linspace(0, 10, 100)
+    
+    # 计算并绘制模型曲线
+    model.plot_model(time)
+    
+    # 加载实验数据
+    time_data, viral_load_data = load_hiv_data('HIVseries.npz')
+    
+    # 绘制实验数据
+    plt.scatter(time_data, viral_load_data, label='Experimental Data')
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     main()
